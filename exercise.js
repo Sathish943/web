aa9aa0cbfdb543fbd905028136d87b9eddcd76d9
Sathish943 <65580537+Sathish943@users.noreply.github.com>
2020-09-29 09:04:10 +0530

    Add initial web files

---
+++ exercise.js
@@ -0,0 +1,8 @@
+var height = prompt("Whats your Height?");
+if(height < 150){
+    console.log("Your height is less ")
+}else if(height < 300){
+    console.log("Your height is more");
+}else{
+    console.log("incorect");
+}
